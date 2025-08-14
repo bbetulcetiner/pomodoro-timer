110f7f060d50eb557869fe4ace560646c1466c5f
bbetulcetiner <bbetulcetiner@gmail.com>
2025-08-14 22:19:41 +0300

    The foundations of the project were created, and fonts were added.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nabla, Tourney } from "next/font/google"
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const nabla = Nabla({
+  subsets: ['latin'],
+  weight: '400',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const tourney = Tourney({
+  subsets: ['latin'],
+  weight: '900',
 });
 
 export const metadata: Metadata = {
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
---
+++ components/timer.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+export default function Timer() {
+  return <div className="max-w-sm shadow-xl w-full p-8 rounded-xl" style={{ backgroundColor: "#E1E9C9" }}>
+    <div className="flex items-center justify-between mb-4">
+        <h2 className="text-xl font-medium capitalize" style={{ color: "#393C52" }}>Focus Time</h2>
+        <div className="text-sm text-gray-500">Sessions: 1</div>
+    </div>
+    
+    <div className="text-center text-6xl font-bold mb-6" style={{ fontFamily:"'Tourney', sans-serif", color: "#4A7473"}}>
+        25:00
+    </div>
+    
+    <div className="flex justify-center space-x-4 mb-4 font-bold" style={{ color: "#393C52" }}>
+        <button className="bg-yellow-100 hover:bg-yellow-200 px-5 py-2 rounded-xl ">Pause</button>
+        <button className=" px-5 py-2 rounded-xl" 
+            style={{ backgroundColor: "#FFFFF0"}}
+            onMouseEnter={ (e) => e.currentTarget.style.backgroundColor = "#EEEED9"}
+            onMouseLeave={ (e) => e.currentTarget.style.backgroundColor = "#FFFFF0"}
+        >
+            Reset
+        </button>
+    </div>
+    
+    <div className="flex justify-center">
+        <button className="text-center px-9 rounded-lg"
+            style={{ backgroundColor: "#FFFFFF", color: "#393C52"}}
+            onMouseEnter={ (e) => e.currentTarget.style.backgroundColor = "#F5F8F8"}
+            onMouseLeave={ (e) => e.currentTarget.style.backgroundColor = "#FFFFFF"}
+        >
+            Switch to Break
+        </button>
+    </div>
+    
+    
+    </div>
+}
