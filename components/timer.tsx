adbd8c8875057db4a8c9cc442e9c5511b7a8ac1e
bbetulcetiner <bbetulcetiner@gmail.com>
2025-08-15 13:54:52 +0300

    "switch the break" button and "sessions" section activated, sound added

--- components/timer.tsx
+++ components/timer.tsx
@@ -3,7 +3,8 @@
 import useTimer from "@/hooks/use-timer";
 
 export default function Timer() {
-    const { mode, timeLeft, toggleTimer, isActive, resetTimer } = useTimer();
+    const { mode, timeLeft, toggleTimer, isActive, resetTimer, switchMode, sessions } = useTimer();
+    console.log(mode);
     
     const minutes = Math.floor(timeLeft / 60);
     const seconds = timeLeft % 60;
@@ -11,8 +12,10 @@ export default function Timer() {
 
   return <div className="max-w-sm shadow-xl w-full p-8 rounded-xl" style={{ backgroundColor: "#E1E9C9" }}>
     <div className="flex items-center justify-between mb-4">
-        <h2 className="text-xl font-medium capitalize" style={{ color: "#393C52" }}>{mode} Time</h2>
-        <div className="text-sm text-gray-500">Sessions: 1</div>
+        <h2 className="text-xl font-medium capitalize" style={{ color: "#393C52" }}>
+            {mode === "focus" ? "✍︎ Focus" : "☕︎ Break"}
+        </h2>
+        <div className="text-sm text-gray-500">Sessions: {sessions}</div>
     </div>
     
     <div className="text-center text-6xl font-bold mb-6" style={{ fontFamily:"'Tourney', sans-serif", color: "#4A7473"}}>
@@ -22,28 +25,35 @@ export default function Timer() {
     <div className="flex justify-center space-x-4 mb-4 font-bold" style={{ color: "#393C52" }}>
         <button 
             onClick={toggleTimer} 
-            className="bg-yellow-100 hover:bg-yellow-200 px-5 py-2 rounded-xl "> 
+            className={`px-5 py-2 rounded-xl ${
+                isActive
+                    ? "bg-yellow-100"
+                    : "bg-yellow-200"
+            }`}
+            >
             {isActive ? "Pause" : "Start"}
         </button>
     
         <button 
             onClick={resetTimer} 
             className=" px-5 py-2 rounded-xl" 
-                style={{ backgroundColor: "#FFFFF0"}}
-                onMouseEnter={ (e) => e.currentTarget.style.backgroundColor = "#EEEED9"}
-                onMouseLeave={ (e) => e.currentTarget.style.backgroundColor = "#FFFFF0"}
+            style={{ backgroundColor: "#FFFFF0"}}
+            onMouseEnter={ (e) => e.currentTarget.style.backgroundColor = "#EEEED9"}
+            onMouseLeave={ (e) => e.currentTarget.style.backgroundColor = "#FFFFF0"}
         >
             Reset
         </button>
     </div>
     
     <div className="flex justify-center">
-        <button className="text-center px-9 rounded-lg"
+        <button 
+            onClick={switchMode}
+            className="text-center px-9 rounded-lg"
             style={{ backgroundColor: "#FFFFFF", color: "#393C52"}}
             onMouseEnter={ (e) => e.currentTarget.style.backgroundColor = "#F5F8F8"}
             onMouseLeave={ (e) => e.currentTarget.style.backgroundColor = "#FFFFFF"}
         >
-            Switch to Break
+            Switch to {mode === "focus" ? "Break" : "Focus"}
         </button>
     </div>
     
--- hooks/use-timer.tsx
+++ hooks/use-timer.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 type TimerMode = "focus" | "break";
 const focusTime = 25;
 const breakTime = 5;
@@ -9,10 +9,11 @@ export default function useTimer() {
   const [mode, setMode] = useState<TimerMode>("focus")
   const [timeLeft, setTimeLeft] = useState(focusTime * 60);
   const [isActive, setIsActive] = useState<boolean>(false);
+  const [sessions, setSessions] = useState(0);
 
-  const getDuration = (TimerMode: TimerMode) => {
-    return TimerMode === "focus" ? focusTime * 60 : breakTime * 60;
-  }
+  const getDuration = useCallback((mode: TimerMode) => {
+    return mode === "focus" ? focusTime * 60 : breakTime * 60;
+  }, []);
 
   const resetTimer = () => {
     setIsActive(false);
@@ -23,12 +24,33 @@ export default function useTimer() {
     setIsActive((prev) => !prev);
   };
 
+  const switchMode = useCallback(() => {
+    const newMode = mode === "focus" ? "break" : "focus";
+    setMode(newMode);
+    setTimeLeft(getDuration(newMode));
+    setIsActive(false);
+    
+    if(newMode === "break") {
+      setSessions((prev) => prev + 1);
+    }
+  }, [mode, getDuration]);
+
   useEffect(() => {
   let interval: NodeJS.Timeout | null = null;
     
     if(isActive) {
       interval = setInterval(() => {
         setTimeLeft((prev) => {
+          if (prev <= 1) {
+            clearInterval(interval!);
+
+            try {
+              const audio = new Audio("/new-notification-011-364050.mp3");
+              audio.play().catch(e => console.log("Error playing sound",e));
+            } catch (error) {
+              console.log("Error playing sound",error);
+            }
+          }
           return prev - 1;
         });
       }, 1000);
@@ -42,6 +64,12 @@ export default function useTimer() {
    };
   }, [isActive]);
 
+  useEffect(() => {
+    if(timeLeft === 0) {
+      switchMode();
+    }
+  }, [timeLeft, switchMode]);
+
 
   return {
     mode,
@@ -49,5 +77,7 @@ export default function useTimer() {
     toggleTimer,
     isActive,
     resetTimer,
+    switchMode,
+    sessions,
   };
 }
